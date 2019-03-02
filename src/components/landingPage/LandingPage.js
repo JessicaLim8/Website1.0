6e827b1aa170e313a7e3777cb5b3b5b654e2d7a7
JessicaLim8 <jessicalim@bell.net>
2019-03-02 17:32:21 -0500

    added icon side bar

--- src/components/landingPage/LandingPage.js
+++ src/components/landingPage/LandingPage.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import Typed from 'react-typed';
+import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelopeSquare } from '@fortawesome/free-solid-svg-icons';
+import MediaIcon from './MediaIcon';
 
 class LandingPage extends React.Component {
   render() {
     return (
       <div>
-        <h1 id="heading">
-          <Typed
-            startDelay={500}
-            strings={["Hi I'm Jessica Lim"]}
-            typeSpeed={60}
-          />
-        </h1>
-        <p classname="blurb">
-          blah blah blah paragraph on how i am cool
-        </p>
+        <div classname="mainInfo">
+          <h1 id="heading">
+            <Typed
+              startDelay={500}
+              strings={["Hi I'm Jessica Lim"]}
+              typeSpeed={60}
+            />
+          </h1>
+          <p classname="blurb">
+            blah blah blah paragraph on how i am cool
+          </p>
+        </div>
+        <div classname="iconBar">
+          <MediaIcon icon={faGithubSquare} />
+          <MediaIcon icon={faLinkedin} />
+          <MediaIcon icon={faEnvelopeSquare} />
+        </div>
       </div>
     );
   }
--- src/components/landingPage/MediaIcon.js
+++ src/components/landingPage/MediaIcon.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class MediaIcon extends React.Component {
   render() {
     return (
+      <FontAwesomeIcon icon={this.props.icon} />
     );
   }
 }
