77f4cf9e0b5d01a1996a64664ff7f2a0118d06eb
JessicaLim8 <jessicalim@bell.net>
2019-03-07 00:01:23 -0500

    added hovering over icons

--- src/components/landingPage/LandingPage.js
+++ src/components/landingPage/LandingPage.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Typed from 'react-typed';
 import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelopeSquare } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelopeSquare, faFile } from '@fortawesome/free-solid-svg-icons';
 import MediaIcon from './MediaIcon';
+import './landing.css';
 
 class LandingPage extends React.Component {
   render() {
     return (
-      <div>
-        <div classname="mainInfo">
+      <div className="landing">
+        <div className="mainInfo">
           <h1 id="heading">
             <Typed
               startDelay={500}
@@ -16,14 +17,15 @@ class LandingPage extends React.Component {
               typeSpeed={60}
             />
           </h1>
-          <p classname="blurb">
+          <p className="blurb">
             blah blah blah paragraph on how i am cool
           </p>
         </div>
-        <div classname="iconBar">
+        <div className="iconBar">
           <MediaIcon icon={faGithubSquare} />
           <MediaIcon icon={faLinkedin} />
           <MediaIcon icon={faEnvelopeSquare} />
+          <MediaIcon icon={faFile} />
         </div>
       </div>
     );
--- src/components/landingPage/MediaIcon.js
+++ src/components/landingPage/MediaIcon.js
@@ -2,9 +2,27 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class MediaIcon extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state= {hover: false};
+  }
+
+  onMouseEnter() {
+    this.setState({hover: true});
+  }
+
+  onMouseOut() {
+    this.setState({hover: false});
+  }
+
   render() {
     return (
-      <FontAwesomeIcon icon={this.props.icon} />
+      <FontAwesomeIcon
+        icon={this.props.icon}
+        id = {this.state.hover ? "hover" : "not-hover"}
+        onMouseEnter={this.onMouseEnter.bind(this)}
+        onMouseLeave={this.onMouseOut.bind(this)}
+      />
     );
   }
 }
